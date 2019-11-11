843d0b94bafdafe8846e98b756eaca20e63ec664
Logan McNulty <logan.mcnulty@zmfs.local>
2019-11-11 16:09:36 -0500

    bug fixe, all friends not displaying scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,38 +2,38 @@ var allUsers = [
     {
       userName: "logan",
       userImage: "https://image.shutterstock.com/image-vector/simple-cartoon-wizard-staff-isolated-260nw-104091740.jpg",
-      surveyAnswers: [1,3,1,4,4,2,2,1,1,1],
-      surveyScore: [20]
+      surveyAnswers: [1,1,1,1,1,1,1,1,1,1],
+      surveyScore: [10]
     },
     {
       userName: "stephanie",
       userImage: "https://img.pixers.pics/pho_wat(s3:700/FO/48/93/29/95/700_FO48932995_4fa7d57113707b6771a5256d6738325f.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/wall-murals-illustration-of-cartoon-witch-with-broomstick.jpg.jpg",
-      surveyAnswers: [1,4,2,3,2,2,4,5,5,3],
-      surveyScore: [31]
+      surveyAnswers: [2,2,2,2,2,2,2,2,2,2],
+      surveyScore: [20]
     },
     {
       userName: "sean",
       userImage: "https://previews.123rf.com/images/toonerman/toonerman1403/toonerman140300090/26843648-cartoon-wizard-illustration.jpg",
-      surveyAnswers: [2,4,4,3,2,1,5,3,4,5],
-      surveyScore: [33]
+      surveyAnswers: [3,3,3,3,3,3,3,3,3,3],
+      surveyScore: [30]
     },
     {
       userName: "will",
       userImage: "https://i.pinimg.com/originals/e9/13/77/e91377c57e188ebf46146f9607a589be.jpg",
-      surveyAnswers: [5,4,2,3,3,2,2,3,3,3],
-      surveyScore: [30]
+      surveyAnswers: [4,4,4,4,4,4,4,4,4,4],
+      surveyScore: [40]
     },
     {
       userName: "karla",
       userImage: "https://cdn1.iconfinder.com/data/icons/halloween-doodles-2/512/doodle_cute_cartoon_witch_broom-512.png",
-      surveyAnswers: [1,5,2,2,3,4,2,5,4,3],
-      surveyScore: [31]
+      surveyAnswers: [5,5,5,5,5,5,5,5,4,5],
+      surveyScore: [50]
     },
     {
       userName: "kyle",
       userImage: "https://previews.123rf.com/images/krisdog/krisdog1703/krisdog170300183/74903534-a-cute-cartoon-wizard-mascot-character-pointing-.jpg",
-      surveyAnswers: [1,2,2,3,5,2,1,2,2,1],
-      surveyScore: [21]
+      surveyAnswers: [1,2,1,2,1,2,1,2,1,2],
+      surveyScore: [15]
     },
   ]
 
